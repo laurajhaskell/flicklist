a1b37137596f0149b5c69c125ac21432b2bd9c8a
Laura Haskell <laurajhaskell@yahoo.com>
2019-01-12 13:51:17 -0600

    finished studio3

--- js/flicklist.js
+++ js/flicklist.js
@@ -9,7 +9,7 @@ var model = {
 var api = {
 
   root: "https://api.themoviedb.org/3",
-  token: "TODO", // TODO 0 add your api key
+  token: "bf458838a3749e0b438dd954439b2c13", // TODO 0 add your api key
 
   /**
    * Given a movie object, returns the url to its poster image
@@ -17,8 +17,8 @@ var api = {
   posterUrl: function(movie) {
     // TODO 4b
     // implement this function
-
-    return "http://images5.fanpop.com/image/photos/25100000/movie-poster-rapunzel-and-eugene-25184488-300-450.jpg" 
+    var baseImageUrl = "http://image.tmdb.org/t/p/w300/";
+    return baseImageUrl + movie.poster_path;
   }
 }
 
@@ -76,24 +76,38 @@ function render() {
 
   // insert watchlist items
   model.watchlistItems.forEach(function(movie) {
-    var title = $("<h6></h6>").text(movie.original_title);
+    var title = $("<h6></h6>")
+      .text(movie.original_title);
 
     // TODO 1 
     // add an "I watched it" button and append it below the title
     // Clicking should remove this movie from the watchlist and re-render
-
-    // TODO 2i
-    // apply the classes "btn btn-danger" to the "I watched it button"
-
+    var button = $("<button></button>")
+      .text("I watched it")
+      .click(function() {
+        var index = model.watchlistItems.indexOf(movie);
+        model.watchlistItems.splice(index, 1);
+        render();
+      })
+      // TODO 2i
+      // apply the classes "btn btn-danger" to the "I watched it button"
+      .attr("class", "btn btn-danger");
     // TODO 4a
     // add a poster image and append it inside the 
     // panel body above the button
+    var poster = $("<img></img>")
+      .attr("src", api.posterUrl(movie))
+      .attr("class", "img-responsive");
 
+    var panelBody = $("<div></div>")
+      .attr("class", "panel-body")
+      .append(poster)
+      .append(button);
     // TODO 2g
     // re-implement the li as a bootstrap panel with a heading and a body
     var itemView = $("<li></li>")
-      .append(title)
-      .attr("class", "item-watchlist");
+      .append(panelBody)
+      .attr("class", "panel panel-default");
 
     $("#section-watchlist ul").append(itemView);
   });
@@ -116,6 +130,7 @@ function render() {
         render();
       })
       .prop("disabled", model.watchlistItems.indexOf(movie) !== -1);
+      .attr("class", "btn btn-primary");
 
     var overview = $("<p></p>").text(movie.overview);
 
